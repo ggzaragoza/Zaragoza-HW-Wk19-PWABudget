3c5916e4956391d18184c75cb2f199876a7fee34
Greg Zaragoza <greg.g.zaragoza@gmail.com>
2021-12-13 14:50:49 -0600

    Began editing config.js file, added db.js for recording transactions offline

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const config = {
+    entry: "./index.js",
+    output: {
+      path: __dirname + "/dist",
+      filename: "bundle.js"
+    },
+    mode: "production"
+  };
+  
+  module.exports = config;
+  
